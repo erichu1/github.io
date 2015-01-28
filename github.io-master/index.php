194646c3f46f943a9b9d4ff3d981f530dc6b6cf8
Eric Hutchison <erichutch@erichutchison.net>
2015-01-28 05:48:06 -0600

    update git home

---
+++ github.io-master/index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo <!doctype html>
+echo <<<dotest
+<html>
+<head>
+<title><h2>My mine you Yours</h2></title>
+</head>
+<body>
+<div class=mygithome>
+<form action=index.php >
+<input type=text value="addtest">
+<input type=submit value="enter">
+</form>
+</div>
+</body>
+</html>;
+<<<dotest
+?>
+
